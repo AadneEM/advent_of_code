0e43f1bbfd371ea1fed4a0fbd2568015911aa93d
Ådne E. Moldesæter <aadne.e.m@outlook.com>
2021-12-17 23:51:20 +0100

    Day 2 part 2

--- bin/solutions/day02.dart
+++ bin/solutions/day02.dart
@@ -6,12 +6,14 @@ void main(List<String> arguments) {
   List<String> commands = input.split('\n');
 
   print('Advent of code day 2:');
-  print('Part 1: ${partOne(commands)}');
+  print('Part 1: ${solve(commands)}');
+  print('Part 2: ${solve(commands, part2: true)}');
 }
 
-int partOne(List<String> commands) {
+int solve(List<String> commands, {bool part2 = false}) {
   final regExp = RegExp(r'(\w+)\s(\d)');
   int hori = 0;
+  int aim = 0;
   int depth = 0;
 
   for (final command in commands) {
@@ -25,12 +27,23 @@ int partOne(List<String> commands) {
     switch (c) {
       case 'forward':
         hori += amount;
+        if (part2) {
+          depth += aim * amount;
+        }
         break;
       case 'down':
-        depth += amount;
+        if (!part2) {
+          depth += amount;
+        } else {
+          aim += amount;
+        }
         break;
       case 'up':
-        depth -= amount;
+        if (!part2) {
+          depth -= amount;
+        } else {
+          aim -= amount;
+        }
         break;
       default:
         print("Unsupported command '$c'");
