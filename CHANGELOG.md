aaadf2b95f03379b1e085aa32a530d23922cd0bb
Ådne E. Moldesæter <aadne.e.m@outlook.com>
2021-12-17 22:29:38 +0100

    Day 1 part 2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+- Added solution for day 1
+
 ## 1.0.0
 
 - Initial project structure.
--- bin/solutions/day01.dart
+++ bin/solutions/day01.dart
@@ -6,7 +6,8 @@ void main(List<String> arguments) {
   List<int> values = input.split('\n').map(int.parse).toList();
 
   print('Advent of code day 1:');
-  print('Part 1: ${partOne(values)}');
+  // print('Part 1: ${partOne(values)}');
+  print('Part 2: ${partTwo(values)}');
 }
 
 int partOne(List<int> values) {
@@ -17,6 +18,27 @@ int partOne(List<int> values) {
       if (currentValue > previousValue!) count++;
     }
     previousValue = currentValue;
+    return count;
+  });
+}
+
+int partTwo(List<int> values) {
+  int? previousValue;
+  int index = 0;
+
+  return values.fold<int>(0, (count, currentValue) {
+    var treeSum = 0;
+
+    if ((index + 2) >= values.length) return count;
+
+    treeSum = currentValue + values[index + 1] + values[index + 2];
+    if (previousValue != null) {
+      if (treeSum > previousValue!) count++;
+    }
+
+    previousValue = treeSum;
+    index++;
+
     return count;
   });
 }
