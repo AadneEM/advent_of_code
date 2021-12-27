65c3d8170824d8827a43f2a4f1d4bc3c0a432ba3
Ådne E. Moldesæter <aadne.e.m@outlook.com>
2021-12-27 16:46:55 +0100

    Day 3 part 2

--- bin/solutions/day03.dart
+++ bin/solutions/day03.dart
@@ -7,6 +7,7 @@ void main(List<String> arguments) {
 
   print('Advent of code day 2:');
   print('Part 1: ${solve(bytes)}');
+  print('Part 2: ${solve2(bytes)}');
 }
 
 int solve(List<List<int>> bytes) {
@@ -22,4 +23,22 @@ int solve(List<List<int>> bytes) {
   int epsilon = int.parse(leastCommonBits.join(), radix: 2);
 
   return gamma * epsilon;
+}
+
+int solve2(List<List<int>> bytes) {
+  final List<List<int>> oxygen = List.from(bytes);
+  final List<List<int>> co2 = List.from(bytes);
+
+  for (int i = 0; i < bytes.first.length; i++) {
+    final mostCommon = (oxygen.fold<int>(0, (a, b) => a + b[i]) / oxygen.length).round();
+    final leastCommon = (co2.map((e) => e[i]).toList()..removeWhere((e) => e != 1)).length < (co2.length / 2) ? 1 : 0;
+
+    if (oxygen.length > 1) oxygen.removeWhere((e) => e[i] != mostCommon);
+    if (co2.length > 1) co2.removeWhere((e) => e[i] != leastCommon);
+  }
+
+  int o = int.parse(oxygen.first.join(), radix: 2);
+  int c = int.parse(co2.first.join(), radix: 2);
+
+  return o * c;
 }
